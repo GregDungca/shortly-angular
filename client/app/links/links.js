3fdda0f3b1f72b89e62795eedd6bfc1961d3310c
GregDungca <greg.dungca@gmail.com>
2015-11-06 16:59:31 -0800

    Passed routing tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,14 +3,14 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
 
-  $scope.getLinks = function() {
+  $scope.getLinks = function () {
     Links.getLinks()
       .then( function successCallback(response) {
         $scope.data.links = response.data;
       }, function errorCallback(response) {
         console.log('Error: ' + response);
       });
-  }
+  };
 
   $scope.getLinks();
 
